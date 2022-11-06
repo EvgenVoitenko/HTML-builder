00deff49c55eb8e92f173deb4b01eb4a4ebd76f6
EvgenVoitenko <evgenvoitenko@gmail.com>
2022-11-06 18:37:11 +0300

    feat: add script tack 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,14 @@
-
+const fs = require('fs');
+const path = require('path');
+async function f() {
+    await fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
+        if (err) throw console.log('Папка не создана')
+    })
+    let fileList = await fs.promises.readdir(path.join(__dirname, 'files'), { withFileTypes: true });
+    fileList.forEach(fileName =>
+        fs.copyFile(path.join(__dirname, 'files', `${fileName.name}`), path.join(__dirname, 'files-copy', `${fileName.name}`), err => {
+            if (err) console.log('ошибка при копировании')
+        })
+    )
+}
+f()
